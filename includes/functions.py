f6e2b4e531fa33e90c861b4d1b954eadf635dec5
Bheesham Persaud <bheesham123@hotmail.com>
2011-07-18 14:54:21 -0400

    Tried to add filesonic.com again... and failed... again.

--- includes/functions.py
+++ includes/functions.py
@@ -1,18 +1,12 @@
 # Copyright (C) 2011 Bheesham Persaud
 # The license is available in LICENSE
-import urllib
+import urllib, httplib
 from itertools import izip_longest
 
 # change the user agent
 class AppURLopener( urllib.FancyURLopener ):
 	version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.98 Safari/534.13' # google chrome
 urllib._urlopener = AppURLopener()
-
-def get_page( url ):
-	client = urllib.urlopen( url )
-	page_html = client.read()
-	if client.code != 200: return 0
-	return page_html
 
 def unique(seq, idfun=None):
     # order preserving 
